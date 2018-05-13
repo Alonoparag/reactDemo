eda5e1574c869a8802a9325f861347ee940ff3c4
ALON PARAG <allon.parag@gmail.com>
2018-05-13 18:19:21 +0300

    Added Ingredients

--- react/recipe-props-starter/src/Recipe.js
+++ react/recipe-props-starter/src/Recipe.js
@@ -1,1 +1,16 @@
 import React, {Component} from 'react';
+
+class Recipe extends Component{
+  render(){
+    const {title} = this.props;
+    const ingredients = this.props.ingredients.map((ing, index) =>(<li key={index}>{ing}</li>));
+    retrun(
+      <div>
+        {title}
+        <ul>
+          {ingredients}
+        </ul>
+      </div>
+    )
+  }
+}
