34a92d029584d0e9a3df3ad54a5f9d6a88c74de7
ALON PARAG <allon.parag@gmail.com>
2018-05-13 18:34:54 +0300

    Pasta Pic

--- react/recipe-props-starter/src/RecipeApp.js
+++ react/recipe-props-starter/src/RecipeApp.js
@@ -6,7 +6,11 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-       <Recipe title = "pasta" ingredients ={['flour', 'water']} />
+       <Recipe title = "pasta" ingredients ={['flour', 'water']}
+         instructions="Mix ingredients"
+         img= "pasta.jpg"
+        />
+
       </div>
     );
   }
