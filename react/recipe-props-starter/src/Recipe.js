21eda91c04f988d97595084c531d6ff7c689b942
ALON PARAG <allon.parag@gmail.com>
2018-05-13 18:48:57 +0300

    Styling for recipe card

--- react/recipe-props-starter/src/Recipe.js
+++ react/recipe-props-starter/src/Recipe.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
+import './Recipe.css';
 
 class Recipe extends Component{
   render(){
     const {title, img, instructions} = this.props;
     const ingredients = this.props.ingredients.map((ing, index) =>(<li key={index}>{ing}</li>));
     return(
-      <div>
-        {title}
-        <ul>
-          {ingredients}
-        </ul>
-        <p>{instructions}</p>
-        <img src={img} alt={title}/>
+      <div className = "recipe-card">
+        <div className="recipe-card-img">
+          <img src={img} alt={title}/>
+        </div>
+        <div className="recipe-card-content">
+          <h3>{title}</h3>
+          <ul>
+            <h4 className="recipe-title">Ingredients:</h4>
+            {ingredients}
+          </ul>
+          <h4>Instructions:</h4>
+          <p>{instructions}</p>
+        </div>
       </div>
     )
   }
