8c3126c477b719082d49feaa840743f2b7c6b45a
ALON PARAG <allon.parag@gmail.com>
2018-05-13 18:30:40 +0300

    bla

--- react/recipe-props-starter/src/Recipe.js
+++ react/recipe-props-starter/src/Recipe.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 
 class Recipe extends Component{
   render(){
-    const {title} = this.props;
+    const {title, img, instructions} = this.props;
     const ingredients = this.props.ingredients.map((ing, index) =>(<li key={index}>{ing}</li>));
-    retrun(
+    return(
       <div>
         {title}
         <ul>
           {ingredients}
         </ul>
+        <p>{instructions}</p>
+        <img src={img} alt={title}/>
       </div>
     )
   }
