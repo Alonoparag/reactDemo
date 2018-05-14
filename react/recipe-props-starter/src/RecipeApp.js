607ad5a7b3ac560901a715efea5d3413d1ac10a1
ALON PARAG <allon.parag@gmail.com>
2018-05-14 15:02:46 +0300

    Navbar

--- react/recipe-props-starter/src/RecipeApp.js
+++ react/recipe-props-starter/src/RecipeApp.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import RecipeList from './recipeList/RecipeList'
+import Navbar from './recipeList/Navbar';
+import RecipeList from './recipeList/RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <RecipeList/>
 
       </div>
--- react/recipe-props-starter/src/recipeList/IngredientList.js
+++ react/recipe-props-starter/src/recipeList/IngredientList.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';// eslint-disable-next-line
 import PropTypes from 'prop-types';
 
 class IngredientList extends Component{
-  
+  static defaultProps = {
+    ingredients: []
+  }
   render(){
     return(
       <ul>
