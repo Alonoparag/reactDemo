9d35bfa81496cd526811986f4ff388b0cf10dfd6
ALON PARAG <allon.parag@gmail.com>
2018-05-17 19:06:57 +0300

    changed key to app.state.id

--- react/recipe-props-starter/src/RecipeApp.js
+++ react/recipe-props-starter/src/RecipeApp.js
@@ -8,24 +8,28 @@ class RecipeApp extends Component {
     super(props);
     this.state = {
       recipes:[{
+          id: 1,
           title: "Pasta",
           ingredients: ['flour', 'water'],
           instructions: "Mix ingredients",
           img: "pasta.jpg"
         },
         {
+          id: 2,
           title: "Roasted Cauliflwoer with Tahini",
           ingredients: ['1 meduim cauliflower', '150 ml raw tahini', '200ml water', '2 cloves of garlic', 'lemon juice', 'salt', 'pepper'],
           instructions: "Preheat oven to 180&deg;. Break the cauliflower to small bits, the size of your pinky knuckle. You may cut and use the branches if you like. Prepare a baking tray with baking paper, spread the cauliflower evenly on the tray and drizzle some olive oil on it Roast for 15-20 minutes, or untill the cauliflower has a light bronze color and is softened Mix the tahini sauce ingredients to create a smooth semi-fluid tahini sauce. ",
           img: "cauliflower.jpg"
         },
         {
+          id: 3,
           title: "Smoked Eggplant with Yogurt, Mint and parsley",
           ingredients: ['2 large eggplants', '300ml yogurt', '1 cup mint', '1 cup parsley', '2 cloves of garlic', '2 small cucumbers, the non hydrophonic variety'],
           instructions: "Smoke the eggplants untill the skin can be peeled effortlessly. Peel the eggplants and let them sit over a sieve for 10 minutes, so they will lose excess liquids. Grate the cucumbers, and squeeze the water out of the puree. Mix the ingredients and season with lemon, salt and black pepper.",
           img: "eggyogmint.jpg"
         }
-      ]
+      ],
+      nextRecipeId: 3,
     }
   }
   render() {
--- react/recipe-props-starter/src/recipeList/RecipeList.js
+++ react/recipe-props-starter/src/recipeList/RecipeList.js
@@ -6,7 +6,7 @@ import './RecipeList.css'
 class RecipeList extends Component{
   render(){
     const recipes = this.props.recipes.map((recipe, index) =>{
-      <Recipe key = {index} title = {recipe.title} ingredients = {recipe.ingredients} instructions = {recipe.instructions} img = {recipe.img}/>
+      return <Recipe key = {recipe.id} title = {recipe.title} ingredients = {recipe.ingredients} instructions = {recipe.instructions} img = {recipe.img}/>
     })
     return(
       <div className = "RecipeList">
